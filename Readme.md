76708570f4c2bf89000e450ed0c2fe30f295e004
pevescarol <carol.peves@gmail.com>
2023-03-11 00:31:06 -0300

    se agrega libreria typer

--- Readme.md
+++ Readme.md
@@ -1,5 +1,10 @@
 ## Creando una app con ChatGPT
 
 ### Instalar
-- pip install openai
-- pip install --upgrade openai (en caso de actualizar)
+- pip install openai 
+
+***en caso de actualizar open ai***
+- pip install --upgrade openai 
+
+***libreria para poder crear programas desde la terminal***
+- pip install typer[all]
--- main.py
+++ main.py
@@ -1,31 +1,37 @@
 # Doc: https://platform.openai.com/docs/guides/chat/introduction
 import openai
 import config
+import typer
 
 openai.api_key = config.api_key
 
 
-# contexto del asistente
-messages = [
+def main():
+    # contexto del asistente
+    messages = [
         {"role": "system", "content": "Eres un asistente muy útil."}]
 
-while True:
-    content = input("> Escribe acá tu pregunta...")
+    while True:
+        content = input("> Escribe acá tu pregunta...")
 
-    if content == "exit":
-        break
+        if content == "exit":
+            break
 
-    messages.append({"role": "user", "content": content})
+        messages.append({"role": "user", "content": content})
 
-    response = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages
-    )
+        response = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages
+        )
 
-    # solo quiero imprimir el messsage, osea la respuesta
-    response_content = response.choices[0].message.content
+        # solo quiero imprimir el messsage, osea la respuesta
+        response_content = response.choices[0].message.content
 
-    # le damos contexto al chat para que entienda de que estabamos hablando
-    messages.append({"role": "assistant", "content": response})
+        # le damos contexto al chat para que entienda de que estabamos hablando
+        messages.append({"role": "assistant", "content": response})
 
-    print(response_content)
+        print(response_content)
+
+
+if __name__ == "__main__":
+    typer.run(main)
