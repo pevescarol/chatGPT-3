e19606b2e9290c9bd9a055c3f4c43b0773448644
pevescarol <carol.peves@gmail.com>
2023-03-11 01:10:37 -0300

    formateo en el texto para salida en terminal

--- main.py
+++ main.py
@@ -2,20 +2,34 @@
 import openai
 import config
 import typer
-
-openai.api_key = config.api_key
+from rich import print
+from rich.table import Table
 
 
 def main():
+    openai.api_key = config.api_key
+
+    print("[bold yellow]ChatGPT API con Python[/bold yellow]")
+
+    table = Table("Comando", "Descripción")
+    table.add_row("exit", "Salir de la aplicación")
+    table.add_row("new", "Crear nueva conversación")
+
+    print(table)
+
     # contexto del asistente
-    messages = [
+    context = [
         {"role": "system", "content": "Eres un asistente muy útil."}]
 
-    while True:
-        content = input("> Escribe acá tu pregunta...")
+    messages = [context]
 
-        if content == "exit":
-            break
+    while True:
+        content = __prompt()
+
+        if content == "new":
+            print("Nueva conversación")
+            messages = [context]
+            content = __prompt()
 
         messages.append({"role": "user", "content": content})
 
@@ -30,7 +44,19 @@ def main():
         # le damos contexto al chat para que entienda de que estabamos hablando
         messages.append({"role": "assistant", "content": response})
 
-        print(response_content)
+        print(f"[bold yellow]> [/bold yellow] [green]{response_content}[/green]")
+
+
+def __prompt():
+    prompt = typer.prompt("\n> Escribe acá tu pregunta...")
+
+    if prompt == "exit":
+        exit = typer.confirm("¿Estas segurx?")
+        if exit:
+            print("Hasta luego!")
+            raise typer.Abort()  # se detiene la app de tyoer
+        return  __prompt()
+    return prompt
 
 
 if __name__ == "__main__":
